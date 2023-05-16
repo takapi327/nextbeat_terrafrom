58c6700f29f6cf9a9b4d39717d8895fbdd9a8946
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-05-17 00:02:48 +0900

    Create vpc

---
+++ accounts/productA/vpc.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "product_a_vpc" {
+  cidr_block           = var.vpc_ips.productA
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  instance_tenancy     = "default"
+
+  tags = {
+    Name: "productA"
+  }
+}
