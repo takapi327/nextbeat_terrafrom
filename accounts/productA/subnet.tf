1a0d99a2a6960011e2bb3c860c13d5f4675506b0
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-05-17 00:02:54 +0900

    Create subnet

---
+++ accounts/productA/subnet.tf
@@ -0,0 +1,67 @@
+// ---------- [ Global Subnet ] -------------------------------
+resource "aws_subnet" "sn_global_stg_1" {
+  cidr_block              = var.product_a_subnet_ips.a_global
+  vpc_id                  = aws_vpc.product_a_vpc.id
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name: "sn-global-stg-1"
+  }
+}
+
+resource "aws_subnet" "sn_global_stg_2" {
+  cidr_block              = var.product_a_subnet_ips.b_global
+  vpc_id                  = aws_vpc.product_a_vpc.id
+  availability_zone       = "ap-northeast-1d"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name: "sn-global-stg-2"
+  }
+}
+
+resource "aws_subnet" "sn_global_stg_3" {
+  cidr_block              = var.product_a_subnet_ips.c_global
+  vpc_id                  = aws_vpc.product_a_vpc.id
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name: "sn-global-stg-3"
+  }
+}
+
+// ---------- [ Private Subnet ] -------------------------------
+resource "aws_subnet" "sn_private_stg_1" {
+  cidr_block              = var.product_a_subnet_ips.a_private
+  vpc_id                  = aws_vpc.product_a_vpc.id
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name: "sn-private-stg-1"
+  }
+}
+
+resource "aws_subnet" "sn_private_stg_2" {
+  cidr_block              = var.product_a_subnet_ips.b_private
+  vpc_id                  = aws_vpc.product_a_vpc.id
+  availability_zone       = "ap-northeast-1d"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name: "sn-private-stg-2"
+  }
+}
+
+resource "aws_subnet" "sn_private_stg_3" {
+  cidr_block              = var.product_a_subnet_ips.c_private
+  vpc_id                  = aws_vpc.product_a_vpc.id
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name: "sn-private-stg-3"
+  }
+}
