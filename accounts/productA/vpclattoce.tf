3a0b62420822a2b04d43abd136c7a1143f529baf
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-05-17 16:55:13 +0900

    Add vpc_association

--- accounts/productA/vpclattoce.tf
+++ accounts/productA/vpclattoce.tf
@@ -10,6 +10,14 @@ data "terraform_remote_state" "platform" {
   }
 }
 
+resource "aws_vpclattice_service_network_vpc_association" "vpc_association" {
+  vpc_identifier             = aws_vpc.product_a_vpc.id
+  service_network_identifier = data.terraform_remote_state.platform.outputs.microservice_network_id
+  security_group_ids         = [
+    aws_security_group.sg_alb.id
+  ]
+}
+
 resource "aws_vpclattice_service" "product_a_service" {
   name      = "product-a-service"
   auth_type = "AWS_IAM"
