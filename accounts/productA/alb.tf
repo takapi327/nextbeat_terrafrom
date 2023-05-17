4faaf808a557b79848ef76e3b6beeffec07e03c5
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-05-17 17:45:14 +0900

    Create alb

---
+++ accounts/productA/alb.tf
@@ -0,0 +1,118 @@
+resource "aws_alb" "product_a_alb" {
+  name               = "productA"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg_alb.id, aws_vpc.product_a_vpc.default_security_group_id]
+  subnets            = [aws_subnet.sn_global_stg_1.id, aws_subnet.sn_global_stg_2.id]
+
+  enable_deletion_protection = false
+  depends_on = [
+    aws_vpc.product_a_vpc
+  ]
+}
+
+resource "aws_lb_listener" "product_a_alb_80" {
+  load_balancer_arn = aws_alb.product_a_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      status_code  = "403"
+      message_body = "Connection is not allowed."
+    }
+  }
+}
+
+
+resource "aws_lb_target_group" "product_a_alb_target_group" {
+  name                 = "productA-target-group"
+  port                 = 80
+  protocol             = "HTTP"
+  deregistration_delay = "300"
+  proxy_protocol_v2    = false
+  vpc_id               = aws_vpc.product_a_vpc.id
+  target_type          = "ip"
+
+  health_check {
+    path                = "/healthcheck"
+    healthy_threshold   = 5
+    interval            = 30
+    unhealthy_threshold = 2
+    matcher             = "200"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+  }
+}
+
+resource "aws_lb_listener_rule" "product_a_alb_80_rule" {
+  listener_arn = aws_lb_listener.product_a_alb_80.arn
+  priority     = 1
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.product_a_alb_target_group.arn
+  }
+
+  condition {
+    http_header {
+      http_header_name = "X-From-Restriction-Cloudfront"
+      values           = ["xVm!#bGr32.n"]
+    }
+  }
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+}
+
+resource "aws_alb" "product_a_internal_alb" {
+  name               = "productA-internal"
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg_alb.id, aws_vpc.product_a_vpc.default_security_group_id]
+  subnets            = [aws_subnet.sn_global_stg_1.id, aws_subnet.sn_global_stg_2.id]
+
+  enable_deletion_protection = false
+  depends_on = [
+    aws_vpc.product_a_vpc
+  ]
+}
+
+resource "aws_lb_listener" "product_a_internal_alb_80" {
+  load_balancer_arn = aws_alb.product_a_internal_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      status_code  = "403"
+      message_body = "Connection is not allowed."
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "product_a_internal_alb_80_rule" {
+  listener_arn = aws_lb_listener.product_a_internal_alb_80.arn
+  priority     = 1
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.product_a_alb_target_group.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+}
--- accounts/productA/vpclattoce.tf
+++ accounts/productA/vpclattoce.tf
@@ -13,7 +13,7 @@ data "terraform_remote_state" "platform" {
 resource "aws_vpclattice_service_network_vpc_association" "vpc_association" {
   vpc_identifier             = aws_vpc.product_a_vpc.id
   service_network_identifier = data.terraform_remote_state.platform.outputs.microservice_network_id
-  security_group_ids         = [
+  security_group_ids = [
     aws_security_group.sg_alb.id
   ]
 }
