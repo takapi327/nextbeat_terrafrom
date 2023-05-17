c283c20a531c9495967c5c56f4c5af25590efcca
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-05-17 18:20:32 +0900

    Fix variables

--- accounts/productA/variables.tf
+++ accounts/productA/variables.tf
@@ -12,3 +12,13 @@ variable "product_a_subnet_ips" {
   description = "List of subnet IPs managed by the ProductA account"
   type        = map(any)
 }
+
+variable "cloudfront_custom_header" {
+  description = "Custom header values used by CloudFront and ALB"
+  type        = string
+}
+
+variable "home_wifi" {
+  description = "List of Home WIFI IPs"
+  type = map(any)
+}
