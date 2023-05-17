a3a5b847406f376e05e676161fa360d668b49f8f
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-05-17 17:45:26 +0900

    Create iam

---
+++ accounts/productA/iam.tf
@@ -0,0 +1,62 @@
+/**
+ * ECS タスク用ロール
+ */
+data "aws_iam_policy" "ecs_task_execution_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "ecs_task_execution" {
+  source_policy_documents = [
+    data.aws_iam_policy.ecs_task_execution_role_policy.policy
+  ]
+
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "kms:Decrypt",
+      "secretsmanager:GetSecretValue",
+      "sns:ListTopics",
+      "cloudwatch:DescribeAlarms",
+      "lambda:ListFunctions",
+    ]
+  }
+
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "ssm:GetParameter*",
+      "ssmmessages:CreateControlChannel",
+      "ssmmessages:CreateDataChannel",
+      "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      identifiers = ["ecs-tasks.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name               = "ECSExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_policy" "ecs_execution_role_policy" {
+  name   = "ECSExecutionRole"
+  policy = data.aws_iam_policy_document.ecs_task_execution.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy_attachment" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = aws_iam_policy.ecs_execution_role_policy.arn
+}
