748a57b510b6b508c56455b3c371e3e941b2665d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-05-18 19:59:38 +0900

    Add Github Action OIDC Role

--- accounts/productA/iam.tf
+++ accounts/productA/iam.tf
@@ -60,3 +60,77 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy_attachment"
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = aws_iam_policy.ecs_execution_role_policy.arn
 }
+
+/**
+ * GitHub Actions デプロイ用 IAMロール
+ */
+data "http" "github_oidc_http" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
+data "tls_certificate" "this" {
+  url = jsondecode(data.http.github_oidc_http.response_body).jwks_uri
+}
+
+resource "aws_iam_openid_connect_provider" "github_oidc" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.this.certificates[0].sha1_fingerprint]
+}
+
+resource "aws_iam_role" "deploy_github_actions" {
+  name               = "deploy-github-actions"
+  assume_role_policy = data.aws_iam_policy_document.assume.json
+}
+
+resource "aws_iam_role_policy" "deploy_github_actions_policy" {
+  name   = aws_iam_role.deploy_github_actions.name
+  role   = aws_iam_role.deploy_github_actions.id
+  policy = data.aws_iam_policy_document.permission.json
+}
+
+data "aws_iam_policy_document" "permission" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:*"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:GetBucketLocation"]
+    resources = ["arn:aws:s3:::*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:DeleteObject",
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:PutObjectAcl"
+    ]
+    resources = [
+      "arn:aws:s3:::*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "assume" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = [
+        "https://github.com/takapi327/grpc-sample-projects"
+      ]
+    }
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_oidc.arn]
+    }
+  }
+}
