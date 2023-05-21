2bd2d4a583024af4e90219a3b44e75d5fa05a5e0
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-05-21 17:56:48 +0900

    Create variables

---
+++ accounts/productB/variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  description = "AWS region"
+  default     = "ap-northeast-1"
+}
+
+variable "vpc_ips" {
+  description = "List of VPC IPs for accounts managed under Organization"
+  type        = map(any)
+}
+
+variable "product_b_subnet_ips" {
+  description = "List of subnet IPs managed by the ProductA account"
+  type        = map(any)
+}
+
+variable "cloudfront_custom_header" {
+  description = "Custom header values used by CloudFront and ALB"
+  type        = string
+}
+
+variable "home_wifi" {
+  description = "List of Home WIFI IPs"
+  type = map(any)
+}
