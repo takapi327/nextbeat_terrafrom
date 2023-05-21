c78fbf6948293392a1d664705f074fd413a5f4e8
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-05-21 17:42:42 +0900

    Add aws_network_interface

--- accounts/productA/vpc.tf
+++ accounts/productA/vpc.tf
@@ -8,3 +8,15 @@ resource "aws_vpc" "product_a_vpc" {
     Name : "productA"
   }
 }
+
+data "aws_network_interface" "product_a_internal_nlb_network_interface" {
+  filter {
+    name   = "description"
+    values = ["ELB ${aws_alb.product_a_internal_nlb.arn_suffix}"]
+  }
+
+  filter {
+    name   = "subnet-id"
+    values = [aws_subnet.sn_private_stg_1.id, aws_subnet.sn_private_stg_1.id]
+  }
+}
