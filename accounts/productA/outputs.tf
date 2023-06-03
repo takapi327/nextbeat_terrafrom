0ef8587dfd1b6ee26482ddbdb525b592de99b271
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-03 17:11:41 +0900

    Fix output

--- accounts/productA/outputs.tf
+++ accounts/productA/outputs.tf
@@ -2,6 +2,10 @@ output "product_a_vpc_id" {
   value = aws_vpc.product_a_vpc.id
 }
 
+output "product_a_vpc_default_security_group_id" {
+  value = aws_vpc.product_a_vpc.default_security_group_id
+}
+
 output "sn_private_1_id" {
   value = aws_subnet.sn_private_1.id
 }
@@ -16,4 +20,16 @@ output "sn_private_3_id" {
 
 output "sn_private_1_cidr_block" {
   value = aws_subnet.sn_private_1.cidr_block
+}
+
+output "sn_global_1_id" {
+  value = aws_subnet.sn_global_1.id
+}
+
+output "sn_global_2_id" {
+  value = aws_subnet.sn_global_2.id
+}
+
+output "sn_global_3_id" {
+  value = aws_subnet.sn_global_3.id
 }
