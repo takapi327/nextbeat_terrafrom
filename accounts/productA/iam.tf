1487aa3f42b095a8e5235a44276848ebaba53423
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-04 20:15:12 +0900

    Add repository

--- accounts/productA/iam.tf
+++ accounts/productA/iam.tf
@@ -61,7 +61,8 @@ data "aws_iam_policy_document" "assume" {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
       values   = [
-        "repo:takapi327/grpc-sample-projects:*"
+        "repo:takapi327/grpc-sample-projects:*",
+        "repo:takapi327/jvm-measurement-example:*"
       ]
     }
 
