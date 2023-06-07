5c532ac906e28d84439b350455bae99d57f4ddd7
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-06-07 21:21:18 +0900

    Add cloudwatch for lambda

--- accounts/productA/vpclattice/cloudwatch.tf
+++ accounts/productA/vpclattice/cloudwatch.tf
@@ -5,3 +5,7 @@ resource "aws_cloudwatch_log_group" "ecs" {
 resource "aws_cloudwatch_log_group" "vpclattice_service" {
   name = "/vpclattice/service/logs"
 }
+
+resource "aws_cloudwatch_log_group" "lambda_service" {
+  name = "/aws/lambda/${aws_lambda_function.microservice_lambda.function_name}"
+}
